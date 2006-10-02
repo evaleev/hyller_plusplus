5b188c93330ed66039c09e42f1c1bd339ed368a2
evaleev <evaleev@6842a0c1-9941-4178-a1c1-87194c8d9983>
2006-10-02 04:25:06 +0000

    Added kinetic energy expression in r1,r2,r12 coordinates. There is a bug somewhere.

--- matrix.timpl.h
+++ matrix.timpl.h
@@ -9,14 +9,14 @@ namespace hyller {
       coordinates are known. */
   template <typename F>
     double S(const F& bra, const F& ket) {
-    const double result = I(apply_jacobian(bra*ket));
+    const double result = I(apply_jacobian(bra*ket)) * (8.0 * M_PI * M_PI);
     return result;
   }
 
   /** Computes the normalization constant. Uses S(). */
   template <typename F>
     double NormConst(const F& bf) {
-    const double selfoverlap = I(apply_jacobian(bf*bf));
+    const double selfoverlap = I(apply_jacobian(bf*bf)) * (8.0 * M_PI * M_PI);
     const double result = 1.0/std::sqrt(selfoverlap);
     return result;
   }
@@ -43,10 +43,97 @@ namespace hyller {
     result *= -1.0;
     return result;
   }
+
+  /** Matrix element of the kinetic energy operator over unnormalized functions expressed in (r1,r2,r12) coordinates */
+  template <typename F>
+    double T_R1R2R12(const F& bra, const F& ket) {
+
+    const F _m1_0_0(gen_r1r2r12_oper(-1,0,0));
+    const F _m2_0_0(gen_r1r2r12_oper(-2,0,0));
+    const F _0_m1_0(gen_r1r2r12_oper(0,-1,0));
+    const F _0_m2_0(gen_r1r2r12_oper(0,-2,0));
+    const F _0_0_m1(gen_r1r2r12_oper(0,0,-1));
+    const F _0_0_m2(gen_r1r2r12_oper(0,0,-2));
+    
+    double T1 = 0.0;
+    double T2 = 0.0;
+
+    T1 -= ket.alpha * S(bra,_m1_0_0*ket);
+    if (ket.i) {
+      T1 += ket.i*S(bra,_m2_0_0*ket);
+    }
+    T1 -= ket.beta * S(bra,_0_m1_0*ket);
+    if (ket.j) {
+      T1 += ket.j*S(bra,_0_m2_0*ket);
+    }
+    T1 -= ket.gamma * S(bra,_0_0_m1*ket);
+    if (ket.k) {
+      T1 += ket.k*S(bra,_0_0_m2*ket);
+    }
+
+    T1 *= -2.0;
+
+    
+    T2 -= (2.0*ket.alpha*ket.alpha + 2.0*ket.beta*ket.beta + ket.gamma*ket.gamma) * S(bra,ket);
+    if (ket.i) {
+      T2 += 4.0*ket.i*ket.alpha * S(bra,_m1_0_0*ket);
+      if (ket.i > 1) {
+	T2 -= 2.0*ket.i*(ket.i-1) * S(bra,_m2_0_0*ket);
+      }
+    }
+    if (ket.j) {
+      T2 += 4.0*ket.j*ket.beta * S(bra,_0_m1_0*ket);
+      if (ket.j > 1) {
+	T2 -= 2.0*ket.j*(ket.j-1) * S(bra,_0_m2_0*ket);
+      }
+    }
+    if (ket.k) {
+      T2 += 2.0*ket.k*ket.gamma * S(bra,_0_0_m1*ket);
+      if (ket.k > 1) {
+	T2 -= ket.k*(ket.k-1) * S(bra,_0_0_m2*ket);
+      }
+    }
+
+    T2 -= ket.alpha*ket.gamma * (gen_mult_oper(-1,0,1,bra,ket) - gen_mult_oper(-1,2,-1,bra,ket));
+    T2 -= ket.beta*ket.gamma * (gen_mult_oper(0,-1,1,bra,ket) - gen_mult_oper(2,-1,-1,bra,ket));
+
+    if (ket.i) {
+      T2 += ket.i * ket.gamma * (gen_mult_oper(-2,0,1,bra,ket) - gen_mult_oper(-3,2,1,bra,ket));
+    }
+    if (ket.k) {
+      T2 += ket.k * ket.alpha * (gen_mult_oper(-1,0,0,bra,ket) - gen_mult_oper(-1,2,-2,bra,ket));
+    }
+    if (ket.i && ket.k) {
+      T2 -= ket.i * ket.k * (gen_mult_oper(-2,0,0,bra,ket) - gen_mult_oper(-2,2,-2,bra,ket));
+    }
+
+    if (ket.j) {
+      T2 += ket.j * ket.gamma * (gen_mult_oper(0,-2,1,bra,ket) - gen_mult_oper(2,-3,1,bra,ket));
+    }
+    if (ket.k) {
+      T2 += ket.k * ket.beta * (gen_mult_oper(0,-1,0,bra,ket) - gen_mult_oper(2,-1,-2,bra,ket));
+    }
+    if (ket.j && ket.k) {
+      T2 -= ket.j * ket.k * (gen_mult_oper(0,-2,0,bra,ket) - gen_mult_oper(2,-2,-2,bra,ket));
+    }
+
+    const double result = T1 + T2;
+    return result;
+  }
+
+  /** Matrix elements of the kinetic energy in Hylleraas coordinates */
+  template <typename F>
+    double T_Hylleraas(const F& bra, const F& ket);
+
   /** Matrix element of the kinetic energy operator over unnormalized functions. Uses S() and other functions. */
   template <typename F>
     double T(const F& bra, const F& ket) {
-    return 0.0;
+    if (F::Coords == Coordinates::R1R2R12) {
+      return T_R1R2R12(bra,ket);
+    }
+    if (F::Coords == Coordinates::Hylleraas) {
+      return T_Hylleraas(bra,ket);
+    }
   }
 
 };
